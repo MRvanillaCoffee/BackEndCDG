014478c8081718e707042da24831e716fcf5c077
MRvanillaCoffee <misformhee@gmail.com>
2026-07-18 16:02:53 +0700

    add new database

--- database.py
+++ database.py
@@ -4,6 +4,6 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = ("mysql+pymysql://root:rootpassword@mariadb:3306/curriculum_db")
+DATABASE_URL = ("mysql+pymysql://root:rootpassword@mariadb:3306/new_Curriculum_DB")
 
 engine = create_engine(DATABASE_URL)
